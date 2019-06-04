c91db84539dd08be2f833a39ac0393626b5cdbb3
Steven Herbst <sherbst@stanford.edu>
2019-06-04 15:33:10 -0700

    adding missing files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='justag',
+    version='0.0.1',
+    description='Generating JTAG debug hardware from Markdown files.',
+    url='https://github.com/StanfordVLSI/JusTAG',
+    author='Stanford University',
+    packages=['justag'],
+    install_requires=[
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
